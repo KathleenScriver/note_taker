21a6bfc1f4b48a9c7fa8a5c4c9f01bc1e3614652
Kathleen Yruegas <kathleenecn@gmail.com>
2019-01-08 14:44:29 -0700

    Adds test file for TagFilter class.

--- src/TagFilter.js
+++ src/TagFilter.js
@@ -11,10 +11,10 @@ export default class TagFilter extends Component {
   render() {
     return (
       <div className="tag-filter">
-        <button onClick={this.filterNotes} value="work">Work</button>
-        <button onClick={this.filterNotes} value="hobby">Hobby</button>
-        <button onClick={this.filterNotes} value="personal">Personal</button>
-        <button onClick={this.filterNotes} value="all">All</button>
+        <button onClick={this.filterNotes} value="work" className="work">Work</button>
+        <button onClick={this.filterNotes} value="hobby" className="hobby">Hobby</button>
+        <button onClick={this.filterNotes} value="personal" className="personal">Personal</button>
+        <button onClick={this.filterNotes} value="all" className="all">All</button>
       </div>
     )
   }
