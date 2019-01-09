1eb6f43460ca3c660729dd402ea6298e8b994f8d
Kathleen Yruegas <kathleenecn@gmail.com>
2019-01-08 17:27:30 -0700

    Updates

---
+++ precache-manifest.dd31b74756d42d28b181cfd50f83919a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bf2a2d21922ba7f2af4",
+    "url": "/note_taker/static/js/runtime~main.8bf2a2d2.js"
+  },
+  {
+    "revision": "b2f7849442ff9579f2ab",
+    "url": "/note_taker/static/js/main.b2f78494.chunk.js"
+  },
+  {
+    "revision": "1e3e20059fea7e571015",
+    "url": "/note_taker/static/js/1.1e3e2005.chunk.js"
+  },
+  {
+    "revision": "b2f7849442ff9579f2ab",
+    "url": "/note_taker/static/css/main.c5e0cffe.chunk.css"
+  },
+  {
+    "revision": "a3a87ea7c8ee266e3b4a50fc66e753eb",
+    "url": "/note_taker/index.html"
+  }
+];
