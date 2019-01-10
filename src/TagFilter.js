c5db98021a1b7b69a9e4898982e318743dc52110
Kathleen Yruegas <kathleenecn@gmail.com>
2019-01-10 12:45:47 -0700

    Adds date filter.

--- src/TagFilter.js
+++ src/TagFilter.js
@@ -15,6 +15,8 @@ export default class TagFilter extends Component {
         <button onClick={this.filterNotes} value="hobby" className="hobby">Hobby</button>
         <button onClick={this.filterNotes} value="personal" className="personal">Personal</button>
         <button onClick={this.filterNotes} value="all" className="all">All</button>
+        <button onClick={this.filterNotes} value="recent" className="recent">Recent</button>
+        <button onClick={this.filterNotes} value="oldest" className="oldest">Oldest</button>
       </div>
     )
   }
